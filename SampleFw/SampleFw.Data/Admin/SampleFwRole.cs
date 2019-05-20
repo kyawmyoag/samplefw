b6a68ed5981511ea22283e282901f8ef97ea9584
kyawmyo <info.kyawmyo@gmail.com>
2019-05-20 10:59:21 -0700

    Add DbContext & User & Roles

---
+++ SampleFw/SampleFw.Data/Admin/SampleFwRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace SampleFw.Data.Admin
+{
+    public class SampleFwRole : IdentityRole
+    {
+    }
+}
---
+++ SampleFw/SampleFw.Data/Common/SampleFwDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleFw.Data.Common
+{
+    public class SampleFwDbContext : IdentityDbContext
+    {
+        public SampleFwDbContext(DbContextOptions<SampleFwDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
